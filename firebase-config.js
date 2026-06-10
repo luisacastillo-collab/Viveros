176a4fdbed96dc078a8deb6eea06818bf5056c93
luisacastillo-collab <luisa.castillo@wamb.ai>
2026-06-09 19:18:09 -0500

    agregar login con Google y correo/contraseña

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYWcvm19Usd44o2Dqv2eSKY9HwufDpuzM",
+  authDomain: "aniwalks-2e9ec.firebaseapp.com",
+  projectId: "aniwalks-2e9ec",
+  storageBucket: "aniwalks-2e9ec.firebasestorage.app",
+  messagingSenderId: "695717396484",
+  appId: "1:695717396484:web:76e2cf1aebb73e8344f551"
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged };
